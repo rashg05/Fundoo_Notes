000c5bb71da85a1fd8fda80fc80ea5f928693dea
rashg05 <gajra.rashmi0504@gmail.com>
2022-04-06 17:04:01 +0530

    [Rashmi]Modified: User Log In methods modified in project Fundoo Keep

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -11,17 +11,19 @@ import jwt from 'jsonwebtoken';
  */
 export const userAuth = async (req, res, next) => {
   try {
-    let bearerToken = req.header('Authorization');
+    let bearerToken = req.header('token');
     if (!bearerToken)
       throw {
         code: HttpStatus.BAD_REQUEST,
         message: 'Authorization token is required'
       };
-    bearerToken = bearerToken.split(' ')[1];
+    // bearerToken = bearerToken.split(' ')[1];
 
-    const { user } = await jwt.verify(bearerToken, 'your-secret-key');
-    res.locals.user = user;
-    res.locals.token = bearerToken;
+    const user = await jwt.verify(bearerToken, 'your-secret-key');
+      
+      console.log("user credentials: ", user);
+    // res.locals.user = user;
+    // res.locals.token = bearerToken;
     next();
   } catch (error) {
     next(error);
--- src/services/user.service.js
+++ src/services/user.service.js
@@ -3,30 +3,39 @@ import User from '../models/user.model';
  
 var mongoose = require('mongoose');
 var bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
 
 //create new user
 export const userRegistration = async (userBody) => {
+  const data = await User.findOne({email: userBody.email});
+  if(data){
+    throw new Error('User Already Exist with this EmailId');
+  }
+  else{
   let saltRounds = 10;
   const hashPass = await bcrypt.hash(userBody.password, saltRounds)
   userBody.password = hashPass;
-  const data = await User.create(body);
-  return data;
+  const responseData = await User.create(userBody);
+  return responseData;
+  }
 };
 
 //get single user
 export const userLogIn = async (userBody) => {
-  const data = await User.find(userBody.email);
-  console.log(data);
+  const data = await User.findOne({email: userBody.email});
+  
+  console.log("my data: ", data);
 
  if(data != null){
-   const validPassword = bcrypt.compareSync(userBody.password, data.password);
+   const validPassword = bcrypt.compare(userBody.password, data.password);
    console.log(userBody.password);
    console.log(data.password);
    if(validPassword){
-    
-   }  
+    var token = jwt.sign({ email: data.email, id: data._id }, process.env.KEY);
+    return token;
+   }
    else{
-     throw new Error('password does not match');
+     throw new Error('Password does not match');
    }
  }
  else{
